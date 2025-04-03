2673998c143d280589acfc52c56a634ac1e28d0a
oliverlibaw <oliver_libaw@yahoo.com>
2025-04-02 18:42:28 -0700

    Update run script to include system Python packages

--- run.sh
+++ run.sh
@@ -9,5 +9,9 @@ source "$DIR/cat_venv/bin/activate"
 # Get the full path to the Python interpreter in the virtual environment
 PYTHON_PATH=$(which python3)
 
+# Add system Python packages to PYTHONPATH
+SYSTEM_PYTHON_PATH="/usr/lib/python3.11:/usr/lib/python3/dist-packages"
+export PYTHONPATH="$SYSTEM_PYTHON_PATH:$PYTHONPATH"
+
 # Run the script with sudo, preserving the virtual environment
 sudo -E env "PATH=$PATH" "PYTHONPATH=$PYTHONPATH" "$PYTHON_PATH" "$DIR/main.py" 
